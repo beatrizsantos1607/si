2c0c1fea43aa50155b726b97856a41504d7044f3
Beatriz <153609527+beatrizsantos1607@users.noreply.github.com>
2024-12-29 19:49:41 +0000

    eheh

--- src/si/metrics/mse.py
+++ src/si/metrics/mse.py
@@ -37,21 +37,3 @@ def mse_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         The derivative of the mean squared error
     """
     return 2 * np.sum(y_true - y_pred) / len(y_true)
-
-def rmse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    """
-    Calculate the Root Mean Squared Error (RMSE).
-
-    Parameters
-    ----------
-    y_true: np.ndarray
-        The true labels
-    y_pred: np.ndarray
-        The predicted labels
-
-    Returns
-    -------
-    rmse: float
-        The root mean squared error
-    """
-    return np.sqrt(np.mean((y_true - y_pred) ** 2))
